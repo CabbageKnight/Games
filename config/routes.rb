e1dc8418995ea7b9c3cb06edfbe16933fe35035a
Anton Chuchkalov <a2new@yandex.ru>
2016-01-31 20:58:35 +0700

    fix bug in route and set up controller method properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :cabbages
-  root 'main_page#index' do
-  post :clear_image, on: :member
-end
+  root 'main_page#index'
+
+  resources :cabbages do
+    post :clear_image, on: :member
+  end
 end
